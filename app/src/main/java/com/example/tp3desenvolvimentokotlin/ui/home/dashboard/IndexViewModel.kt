b5c9a244643868b531276399622bc13340958f0e
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2021-06-06 23:29:29 -0300

    adiciona tela de home

---
+++ app/src/main/java/com/example/tp3desenvolvimentokotlin/ui/home/dashboard/IndexViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.tp3desenvolvimentokotlin.ui.home.dashboard
+
+import androidx.lifecycle.ViewModel
+
+class IndexViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
